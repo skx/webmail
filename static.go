ee8869ad8e8575c1806d2a03de35071910ba5544
Steve Kemp <steve@steve.org.uk>
2018-07-25 18:24:53 +0300

    Fixed ineffective assignment

--- static.go
+++ static.go
@@ -74,6 +74,7 @@ func getResource(path string) ([]byte, error) {
 		//
 		if entry.Filename == path {
 			var raw bytes.Buffer
+			var err error
 
 			// Decode the data.
 			in, err := hex.DecodeString(entry.Contents)
@@ -83,6 +84,9 @@ func getResource(path string) ([]byte, error) {
 
 			// Gunzip the data to the client
 			gr, err := gzip.NewReader(bytes.NewBuffer(in))
+			if err != nil {
+				return nil, err
+			}
 			defer gr.Close()
 			data, err := ioutil.ReadAll(gr)
 			if err != nil {
