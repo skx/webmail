5f2ba877fbd4572d4b57e4ca9a8fa861ccb1c8a4
Martin Ashby <martin@ashbysoft.com>
2020-11-06 23:32:28 +0000

    Removed repetition of javascript in each HTML file. It's now included as a sub-template.
    Parse all the templates upfront on server start, and look them up
    when needed. This
    1. Reduces code duplication loading templates from
    resources and
    2. Saves re-parsing templates on every page load
    
    More work can be done removing duplication from the HTML files and
    stylesheets. Componentizing the HTML like this should eventually allow
    composite views for different sized screens: e.g. including the message
    view and message list on the same page in wide screen formats.

--- static_gen.go
+++ static_gen.go
@@ -1,4 +1,5 @@
 package main
 
-// This file exists to use go generate static.go
+// These annotations describe how generated source files are produced. 
+// `implant` tool can be obtained here: github.com/skx/implant
 //go:generate implant -input data/ -output static.go
