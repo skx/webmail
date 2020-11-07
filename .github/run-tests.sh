6aa7e3d4dba169bbcffbf2b44dbf84714f82ed3c
Steve Kemp <steve@steve.org.uk>
2020-11-07 03:27:26 +0200

    Try to generate `static.go` as part of the build/test
    Since this is now no longer committed in the repository

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -3,5 +3,10 @@
 # init modules
 go mod init
 
+# Ensure that we have `implant` for the moment, to generate
+# static resources.
+go get -u github.com/skx/implant
+go generate
+
 # Run golang tests - we have none.  oops.
 go test ./...
